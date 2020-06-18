6359f834e08499c14f8731de91cd7b8251af8dd6
nye0 <yeningrong@csu.edu.cn>
2020-06-18 20:32:48 +0800

    edit the input format

--- src/FiberEstimation.sh
+++ src/FiberEstimation.sh
@@ -1,15 +1,98 @@
 #!/bin/bash
-PreprocessedImage=$1
-BrainMask=$2
-T1Raw=$3
-ResultRoot=$4
 #PreprocessedImage=result/testing/raw-den-unr-mdc-unbiased.mif
 #BrainMask=/Users/ningrongye/Desktop/python/MRI/TCB/Test_mrtrix/FSL_brainmask.mif
 #T1Raw=data/FT_T1/
 #ResultRoot=result/FiberEstimation/
-if [ ! -d "${ResultRoot}" ]; then
-	mkdir -p $ResultRoot
+
+get_batch_options(){
+        local arguments=("$@")
+        local index=0
+        local numArgs=${#arguments[@]}
+        local argument
+
+        while [ ${index} -lt ${numArgs} ]; do
+                argument=${arguments[index]}
+                case ${argument} in
+                        --PreprocessedImage=*)
+                                PreprocessedImage=${argument#*=}
+                                index=$(( index + 1 ))
+                                ;;
+                        --T1Image=*)
+                                T1Raw=${argument#*=}
+                                index=$(( index + 1 ))
+                                ;;
+                        --BrainMask=*)
+                                # 
+                                BrainMask=${argument#*=}
+                                index=$(( index + 1 ))
+                                ;;
+                        --ResultRoot=*)
+                                ResultRoot=${argument#*=}
+                                index=$(( index + 1 ))
+                                ;;
+                        *)
+                echo ""
+                echo "ERROR: Unrecognized Option: ${argument}"
+                echo ""
+                exit 1
+                ;;
+        esac
+    done
+}
+get_batch_options "$@"
+# check the input
+#----------------------------------------------------------
+if [ -n "${PreprocessedImage}" ]; then
+        if [ -e "${PreprocessedImage}" ]; then
+                echo PreprocessedImage: $PreprocessedImage
+        else
+                echo PreprocessedImage Do not exist!
+                exit
+        fi
+else
+        echo Please set PreprocessedImage!
+        exit
 fi
+if [ -n "${T1Raw}" ]; then
+        if [ -e "${T1Raw}" ]; then
+                echo T1Image: $T1Raw
+        else    
+                echo T1Image Do not exist!
+                exit
+        fi      
+else    
+        echo Please set T1Image!
+        exit
+fi    
+if [ -n "${BrainMask}" ]; then
+        if [ -e "${BrainMask}" ]; then
+                echo BrainMask: $BrainMask
+        else    
+                echo BrainMask Do not exist!
+                exit
+        fi      
+else            
+        echo Please set BrainMask!
+        exit
+fi
+if [ -n "${ResultRoot}" ]; then
+        ((checked+=1))
+        echo ResultRoot: $ResultRoot
+        if [ ! -d "${ResultRoot}" ]; then
+                mkdir -p ${ResultRoot}
+        fi
+else
+        echo Please Set ResultRoot!
+        exit
+fi
+#----------------------------------------------------------
+
+
+
+
+
+
+
 # Response Function (RF) estimation
 wm_rf=$ResultRoot/rf_wm.txt
 gm_rf=$ResultRoot/rf_gm.txt
