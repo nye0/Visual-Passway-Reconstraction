2ad5de55b727a31702e0530fb9a192cc8d5ff18f
nye0 <yeningrong@csu.edu.cn>
2020-06-20 13:13:29 +0800

    tested version 0.1

--- src/TrackGeneration.sh
+++ src/TrackGeneration.sh
@@ -4,12 +4,20 @@ seed=$2
 include=$3
 wmfod_norm=$4
 ResultRoot=$5
-track_10mio=$ResultRoot/track_10mio.tck
+select=5000
+ReduceF=10
+mkdir -p $ResultRoot
+track=$ResultRoot/track_S.${select}.tck
+track_reduced=${track%%.tck}-red.${ReduceF}.tck
 tckgen  -act $tt5 \
 	-backtrack \
 	-seed_image $seed \
 	-include $include \
-	-select 1000000 \
-	$wmfod_norm $track_10mio
+	-select $select \
+	$wmfod_norm $track
+tcksift -act $tt5 \
+	-term_number $(( select/ReduceF )) \
+	$track $wmfod_norm \
+	$track_reduced
 
 
