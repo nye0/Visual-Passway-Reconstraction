8db072f1aaab9d0e4aeb8920b246ee222547b121
nye0 <yeningrong@csu.edu.cn>
2020-07-01 07:41:23 +0800

    modified a little

--- bin/run.sh
+++ bin/run.sh
@@ -14,24 +14,24 @@ ROIUseRoot=$ROIExtraction_ResultRoot/ROIUse/
 
 FS_T1Orig=$SUBJECTS_DIR/$SubjectID/mri/orig/001.nii.gz
 PreprocessedDTI=$DTIpre_ResultRoot/DWI-raw-den-unr-mdc-unbiased.mif
-BrainMask=$DTIpre_ResultRoot/DWI-raw-den-unr-mdc-unbiased-unbrain_mask.mif
+BrainMask=$DTIpre_ResultRoot/DWI-raw-den-unr-mdc-unbiased-unb-fslbet.mif
 FiberEstimation_ResultRoot=result/FiberEstimation/
 
 
-src/DTIpre.sh  --RawImage=data/YQ_DTI_AP/ \
-	       --RawImage_PE=AP \
-	       --RPE=all \
-	       --RPE_Image=data/YQ_DTI_PA/ \
-	       --ResultRoot=./result/DTIpre/ 
-src/ROIExtraction-Opti.V1.LGN.sh --SubjectID=YQ_0006 \
-			    --T1Image=data/YQ_T1_0006/ \
-			    --T2Image=data/YQ_T2_0007/ \
-			    --ResultRoot=/media/oxygen/y2/202002/git/Visual-Passway-Reconstraction/result/ROIExtraction/
+echo src/DTIpre.sh  --RawImage=$RawDWI_AP \
+	       --RawImage_PE=$PE \
+	       --RPE=$RPE \
+	       --RPE_Image=$RPE_Image \
+	       --ResultRoot=$DTIpre_ResultRoot 
+src/ROIExtraction-Opti.LGN.V1.sh --SubjectID=$SubjectID \
+			    --T1Image=$T1Image \
+			    --T2Image=$T2Image \
+			    --ResultRoot=$ROIExtraction_ResultRoot
 
-src/FiberEstimation.sh --PreprocessedImage=result/DTIpre/DWI-raw-den-unr-mdc-unbiased.mif \
-		       --T1Image=../freesurfer_LGNextraction/result/YQ_0006/mri/orig/001.nii.gz \
-		       --BrainMask=result/DTIpre/DWI-raw-den-unr-mdc-unbiased-unbrain_mask.mif \
-		       --ResultRoot=result/FiberEstimation/
+src/FiberEstimation.sh --PreprocessedImage=$PreprocessedDTI \
+		       --T1Image=$FS_T1Orig \
+		       --BrainMask=$BrainMask \
+		       --ResultRoot=$FiberEstimation_ResultRoot 
 # build fiber
 #T1 ROI to DWI
 DWI2T1=$FiberEstimation_ResultRoot/diff2struct-mrtrix.txt
@@ -40,7 +40,7 @@ for h in lh rh; do
 	V1=$ROIUseRoot/T1Orig-${h}.V1_exvivo.nii.gz
 	OpticalNerve=$ROIUseRoot/T1Orig-${h}.OptialNerve.nii.gz
 	for roi in $LGN $V1 $OpticalNerve; do
-		src/ROI2DWI.sh $roi $DWI2ROI
+		src/ROI2DWI.sh $roi $DWI2T1
 	done
 	LGNUse=${LGN%%.nii.gz}-DWI.mif
 	V1Use=${V1%%.nii.gz}-DWI.mif
@@ -53,20 +53,20 @@ for h in lh rh; do
 	TrackName_Op2LGN=${h}.OpticalNerve2LGN
 	TrackName_LGN2V1=${h}.LGN2V1
 	TrackGeneration_ResultRoot=result/TrackGeneration/
-	src/TrackGeneration.sh --act $tt5 \
-	       	               --seed_roi $OpticalNerveUse \
-			       --include_roi $LGNUse \
-			       --wm_fodnorm $wm_fodnorm \
-			       --ResultRoot $TrackGeneration_ResultRoot \
-			       --TrackName $TrackName_Op2LGN \
-			       --TrackMaxLength $TrackMaxLength_Op2LGN
-	src/TrackGeneration.sh --act $tt5 \
-			       --seed_roi $LGNUse \
-			       --include_roi $V1Use \
-			       --wm_fodnorm $wm_fodnorm \
-                               --ResultRoot $TrackGeneration_ResultRoot \
-                               --TrackName $TrackName_LGN2V1 \
-			       --TrackMaxLength $TrackMaxLength_LGN2V1
+	src/TrackGeneration.sh --act=$tt5 \
+	       	               --seed_roi=$OpticalNerveUse \
+			       --include_roi=$LGNUse \
+			       --wm_fodnorm=$wm_fodnorm \
+			       --ResultRoot=$TrackGeneration_ResultRoot \
+			       --TrackName=$TrackName_Op2LGN \
+			       --TrackMaxLength=$TrackMaxLength_Op2LGN
+	src/TrackGeneration.sh --act=$tt5 \
+			       --seed_roi=$LGNUse \
+			       --include_roi=$V1Use \
+			       --wm_fodnorm=$wm_fodnorm \
+                               --ResultRoot=$TrackGeneration_ResultRoot \
+                               --TrackName=$TrackName_LGN2V1 \
+			       --TrackMaxLength=$TrackMaxLength_LGN2V1
 
 	python=/home/oxygen/anaconda3/bin/python
 	VoxelConnection_ResultRoot=result/VoxelConnection/
